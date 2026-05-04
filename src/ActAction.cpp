7cc237fc16c9eadc8195cc8ba0037718d29737de
Aurélien <aurelienissy18@gmail.com>
2026-05-04 19:38:29 +0200

    re test

--- src/ActAction.cpp
+++ src/ActAction.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 ActAction::ActAction(int id, const string& text, int impact)
-    : id(id), displayText(textfvfvf), mercyImpact(impact) {}
+    : id(id), displayText(text), mercyImpact(impact) {}
 
 int         ActAction::getId()          const { return id; }
 string ActAction::getDisplayText() const { return displayText; }
